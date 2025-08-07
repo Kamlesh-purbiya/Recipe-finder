0b17c94f87bb7be4a19f26ac5c69ec933f49e1c2
mr.mewadi <kamleshpurbiya2004@gmail.com>
2025-08-07 19:39:42 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeFinder from './RecipeFinder';
 import RecipeDetails from './RecipeDetails';
 import './App.css';
